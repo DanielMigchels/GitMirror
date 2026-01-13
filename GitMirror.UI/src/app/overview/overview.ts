44b7db1fa27eba6b454144aebe617840e0229499
Daniel <danielm12@hotmail.nl>
2026-01-13 17:30:58 +0100

    Tweaked front-end

--- GitMirror.UI/src/app/overview/overview.ts
+++ GitMirror.UI/src/app/overview/overview.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgIcon } from "@ng-icons/core";
 
 @Component({
   selector: 'app-overview',
-  imports: [NgIcon],
+  imports: [NgIcon, RouterLink],
   templateUrl: './overview.html',
   styleUrl: './overview.css',
 })
